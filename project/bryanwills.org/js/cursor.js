7ee981114217e6c21a90b89fe077d87475a4f567
Bryan Wills <bryanwi09@gmail.com>
2021-07-26 03:25:45 -0400

    Adding cursor animation example files in cursor.html/.css/.js

--- project/bryanwills.org/js/cursor.js
+++ project/bryanwills.org/js/cursor.js
@@ -1,8 +1,13 @@
+
+/* Get the cursor div from site */
 let mouseCursor = document.querySelector(".cursor");
+/* Get all of the links in the navigation */
 let navLinks = document.querySelectorAll(".nav-links li");
 
+/* Listen for all events for the 'mousemove' when the user moves the mouse */
 window.addEventListener('mousemove', cursor);
 
+/* Write this into a function that will also act as an event that will grab the coordinates of the mouse and show the exact position on the screen if you use a console.log(e) Used the pageY and PageX events from the console. The 'e' is the event aka mousemove by user*/
 function cursor(e) {
     mouseCursor.style.top = e.pageY + 'px';
     mouseCursor.style.left = e.pageX + 'px';
